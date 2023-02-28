ede2ecbce447e57c95756eddef00dfebc6b8c0f8
Nick-XYZ <123240062+Nick-XYZ@users.noreply.github.com>
2023-02-28 16:44:42 +0100

    Nicks klick link

---
+++ src/main/java/org/hordekodning/alltid/hordekodare/link/Link.java
@@ -0,0 +1,23 @@
+package org.hordekodning.alltid.hordekodare.link;
+
+
+public class Link {
+
+    private String name;
+    private String url;
+
+    public Link(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
+
+
--- src/main/java/org/hordekodning/alltid/hordekodare/link/LinkRepository.java
+++
@@ -1,8 +0,0 @@
-package org.hordekodning.alltid.hordekodare.link;
-
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class LinkRepository {
-}
