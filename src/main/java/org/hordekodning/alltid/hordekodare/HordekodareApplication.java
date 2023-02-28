fe629bdce12bbbfa8e90945e944f237fe500dffa
atomragnar <johanneshornbrinck@gmail.com>
2023-02-28 10:50:10 +0100

    Initial commit

---
+++ src/main/java/org/hordekodning/alltid/hordekodare/HordekodareApplication.java
@@ -0,0 +1,13 @@
+package org.hordekodning.alltid.hordekodare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HordekodareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HordekodareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hordekodning/alltid/hordekodare/HordekodareApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hordekodning.alltid.hordekodare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HordekodareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
